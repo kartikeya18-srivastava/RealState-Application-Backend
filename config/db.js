e2fa9bd62b2fe174019bf6cfbc51c0bfe0d23f55
kartikeya18-srivastava <kartikeyasrivastava3182@gmail.com>
2025-08-22 22:58:35 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose"
 export const dbConnect=async()=>{
-   const conn= await  mongoose.connect(process.env.MONGO.URI);
+   const conn= await  mongoose.connect(process.env.MONGO_URI);
 
    if(conn){
     console.log("Db connected successfully............"); 
